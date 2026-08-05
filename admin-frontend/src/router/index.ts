73a41a089bb293a00fc75dcc3772a4f2a9b57f6a
AndrewChen666 <andrew105092@gmail.com>
2026-08-06 03:10:53 +0800

    202608060310

--- admin-frontend/src/router/index.ts
+++ admin-frontend/src/router/index.ts
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ADMIN from '../pages/admin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+    path: '/admin',
+    name: 'admin',
+    component: ADMIN
+  }],
 })
 
 export default router
